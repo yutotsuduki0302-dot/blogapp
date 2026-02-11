8dc16f1988dec2fdaae5e5fef64e7051c19f1eea
yuto <yutotsuzuki0302@icloud.com>
2026-02-11 22:01:06 +0900

    artile

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do
+  Article.create(
+    title: Faker::Lorem.sentence(word_count: 5) ,
+    content: Faker::Lorem.sentence(word_count: 100)
+  )
+end
