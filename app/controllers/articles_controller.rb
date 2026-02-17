39193008f139865fc1e73dc7956897b1813c63f8
yuto <yutotsuzuki0302@icloud.com>
2026-02-17 14:53:01 +0900

    show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,5 +3,9 @@ class ArticlesController<ApplicationController
     @articles = Article.all
   end
 
+  def show
+    @article = Article.find(params[:id])
+  end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   root to: 'articles#index'
+  
+  resources :articles, only: [:show]
+
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
